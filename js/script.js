a90078c12a204dd6aace86edb464ce59898739b5
pennpress1 <pennpress1@gmail.com>
2016-03-03 23:34:44 -0500

    new file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   alert('Oh, hi there, world');
-});
+   console.log( "ready!" );
+});*/
+
+$(document).ready(function() {
+	$("#important-events h1").css({
+       backgroundColor: "yellow",
+       color: "red" });
+
+    $("#important-events li:first-child img").hide();
+	$("#important-events > p").css("color","grey");
+	$("#important-events li:first-child").css('background-color', 'steelblue');
+	$("#unimportant-events").fadeIn('slow').delay(1000).hide(0);
+	$("#important-events span").css({
+       backgroundColor: "red",
+       color: "white",
+       padding:"1em" });
+	$("#important-events li:first-child img").fadeIn(5000);
+ });
+
+
+
