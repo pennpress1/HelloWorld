447fea8b8bf84c0854eeac1d9df21df5f6a7f33c
Pennpress1 <Pennpress1@gmail.com>
2016-03-07 20:19:52 -0500

    fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 
-$(document).ready(function() {
+/*$(document).ready(function() {
 	$("#important-events h1").css({
        backgroundColor: "yellow",
        color: "red" });
@@ -26,5 +26,24 @@ $(document).ready(function() {
 	$("#funButton").parent(".button").hide(0);
  });
 
+*/
 
-
+<!--
+    function urlCheck() {
+        if (
+            window.location.href == "/a.html"
+        ) {$("#important-events li:first-child img").hide();
+	       $("#important-events > p").css("color","grey");
+	       $("#important-events li:first-child").css('background-color', 'steelblue');
+	       $("#unimportant-events").fadeIn('slow').delay(1000).hide(0);
+	       $("#important-events span").css({
+                backgroundColor: "red",
+                color: "white",
+                padding:"1em" });
+	       $("#important-events li:first-child img").fadeIn(5000);
+        } else {
+            document.write('<div>testing my super loaded div code sample</div>')
+        }
+    }
+    urlCheck() /* autoLoadFunction */
+        //-->
