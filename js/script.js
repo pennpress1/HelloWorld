a27d7137db751945d72b95306a99698f29a2d325
pennpress1 <pennpress1@gmail.com>
2016-03-29 21:48:38 -0400

    Added files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 
 
-/*$(document).ready(function() {
+$(document).ready(function() {
 	$("#important-events h1").css({
        backgroundColor: "yellow",
        color: "red" });
-
-	   alert('Oh, hi there, world');
-       console.log( "ready!" );
 
     $("#important-events li:first-child img").hide();
 	$("#important-events > p").css("color","grey");
@@ -26,24 +23,5 @@
 	$("#funButton").parent(".button").hide(0);
  });
 
-*/
 
-<!--
-    function urlCheck() {
-        if (
-            window.location.href == "https://pennpress1.github.io/HelloWorld/a.html"
-        ) {$("#important-events li:first-child img").hide();
-	       $("#important-events > p").css("color","grey");
-	       $("#important-events li:first-child").css('background-color', 'steelblue');
-	       $("#unimportant-events").fadeIn('slow').delay(1000).hide(0);
-	       $("#important-events span").css({
-                backgroundColor: "red",
-                color: "white",
-                padding:"1em" });
-	       $("#important-events li:first-child img").fadeIn(5000);
-        } else {
-            document.write('<div>testing my super loaded div code sample</div>')
-        }
-    }
-    urlCheck() /* autoLoadFunction */
-        //-->
+
